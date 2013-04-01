65498693ed505ac3163f1ae847d8f836e5e66384
Andrew Aarestad <andrew@etal-andothers.com>
2013-04-01 11:14:28 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Walripilot::Application.config.secret_token = '315c8004c223c22f765e37b52242cb80ee17a8a0a6081811ce146044e1392812be33c0a5f0471feae7239003b7297acf1edaf2947bffa784acc41f291f8ff8e4'
